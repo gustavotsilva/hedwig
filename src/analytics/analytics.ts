9999c45a7a77f56f4a166319d32107504578992d
Gustavo Silva <me@gustavosilva.io>
2021-03-10 20:37:25 +0000

    Google Tag Manager - Setup

---
+++ src/analytics/analytics.ts
@@ -0,0 +1,9 @@
+const keys = (): IKeys => ({
+    gtm: "GTM-TC9CTDM"
+});
+
+export default keys;
+
+export interface IKeys {
+    gtm: string;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/* Google Tag Manager */
+import TagManager from 'react-gtm-module';
+
 /* Styles */
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './css/fonts.css';
@@ -14,5 +17,9 @@ import '../node_modules/bootstrap/dist/js/bootstrap';
 
 /* Scripts */
 import './js/index';
+import keys from './analytics/analytics'
+
+/* Analytics */
+TagManager.initialize({ gtmId: keys().gtm });
 
 ReactDOM.render(<HomePage />, document.getElementById('root'));
